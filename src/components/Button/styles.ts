3922c53886bb5628cff7fe121ff4d5fbf34a8c7f
Diana Lyvytska <diana.lyvytska@hbsuk.co.uk>
2023-08-21 10:26:04 +0200

    change Button styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,20 +1,20 @@
 import tw from 'twin.macro'
 
-export interface ButtonStyleProps {
-  variant: 'primary' | 'secondary' | 'tertiary'
-  size: 'md' | 'sm'
-}
-
 const sizes = {
   sm: tw`h-8 text-base`,
   md: tw`h-12 text-xl`,
-}
+} as const
 
 const variants = {
   primary: tw`text-zinc-900 rounded-lg bg-amber-400 hover:bg-amber-300 active:bg-amber-500 disabled:bg-zinc-300`,
   secondary: tw`box-border text-zinc-600 outline outline-2 outline-zinc-500 rounded-lg 
     hover:bg-zinc-200 active:bg-zinc-300 disabled:bg-zinc-300`,
   tertiary: tw`text-zinc-500 underline disabled:opacity-50`,
+} as const
+
+export interface ButtonStyleProps {
+  variant: keyof typeof variants
+  size: keyof typeof sizes
 }
 
 const styles = {
